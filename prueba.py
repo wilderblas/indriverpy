68be623b74598e17a0164cc52023e8eb727a356c
wilderblas <xxcreedxx@hotmail.com>
2025-02-02 13:50:05 -0500

    buscando texto en los textContains

--- prueba.py
+++ prueba.py
@@ -10,15 +10,19 @@ def buscar_y_clicar_texto(texto):
     d = u2.connect()  # Conecta al primer dispositivo disponible
     
     # Buscar el texto en la pantalla
-    elemento = d(text=texto)
+    #elemento = d(text=texto)
     
-    if elemento.exists:
+    """ if elemento.exists:
         # Si el texto existe, hacer clic en él
         #elemento.click(500,500)
         print(f"Se encontró el texto")
     else:
         print(f"No se encontró el texto")
-        #elemento.click()
+        #elemento.click() """
+    if d(textContains=texto).exists():
+        print("El texto fue encontrado en la pantalla.")
+    else:
+        print("El texto no fue encontrado.")
 
 #d = u2.connect()
 
@@ -36,18 +40,18 @@ time.sleep(5) """
     time.sleep(0.5) """
 
 
-""" while not carreraTomada:
+while not carreraTomada:
     try:
         buscar_y_clicar_texto("metro")
         time.sleep(2)
     except Exception as e:
         print(f"Ocurrió un error: {e}")
-        break """
+        break
 
-time.sleep(7)
+""" time.sleep(7)
 d = u2.connect()
 hierarchy = d.dump_hierarchy()
 
 # Guardar en un archivo de texto
 with open('hierarchy.xml', 'w', encoding='utf-8') as file:
-    file.write(hierarchy)
+    file.write(hierarchy) """
