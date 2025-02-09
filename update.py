edc18f43076ac79e70538512db043f158bd7403f
wilderblas <xxcreedxx@hotmail.com>
2025-02-09 09:12:38 -0500

    Se crearon 2 archivos para hacer pruebas de reporte de acuerdo a la pantalla

--- update.py
+++ update.py
@@ -6,16 +6,15 @@ import time
 d=u2.connect()
 
 def buscar_y_clicar_texto(texto):
-    d = u2.connect()
-    if d(textContains=texto).exists():
-        d.click(530, 470)
-        time.sleep(0.1)
+    if not d(textContains=texto).exists():
+        d.swipe(500,350,500,1100,duration=0.03)
 
 time.sleep(7)
 
 while True:
     try:
-        buscar_y_clicar_texto("metro")
+        if d(text="Solicitudes de viaje").exists():
+            buscar_y_clicar_texto("metro")
     except Exception as e:
         print(f"Ocurrió un error: {e}")
         break
