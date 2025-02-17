682c5be9e1a42b490aa30503ea961bf35da4c2c6
wilderblas <xxcreedxx@hotmail.com>
2025-02-17 10:50:20 -0500

    Se volvio a programar paara probar si funciona speech_recognize en termux01

--- comandoVoz.py
+++ comandoVoz.py
@@ -1,15 +1,28 @@
-import speech_recognition as sr
+import keyboard
+import uiautomator2 as u2
 import time
+import os
 
 time.sleep(8)
-recognizer = sr.Recognizer()
 
+print("Presiona W, A, S, D o X para ejecutar una acción. Presiona 'Esc' para salir.")
 
-try:
-    with sr.Microphone() as source:
-        print("Escuchando...")
-        audio = recognizer.listen(source)
-        text = recognizer.recognize_google(audio, language="es-ES")
-        print("Dijiste:", text)
-except Exception as e:
-    print("Ocurrió un error:", e)
+# Mapeo de teclas a acciones
+acciones = {
+    "W": print("Aceptar"),
+    "A": print("Opcion1"),
+    "S": print("Opcion2"),
+    "D": print("Opcion3"),
+    "X": print("Cerrar")
+}
+
+while True:
+    event = keyboard.read_event()  # Espera a que se presione una tecla
+    if event.event_type == keyboard.KEY_DOWN:  # Solo detecta cuando la tecla se presiona
+        key = event.name.upper()  # Convierte la tecla a mayúscula
+
+        if key in acciones:
+            acciones[key]  # Imprime la acción correspondiente
+        elif key == "ESC":  # Si presiona "Escape", sale del programa
+            print("Saliendo...")
+            break
